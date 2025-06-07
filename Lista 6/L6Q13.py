2ce14bf52a506a057c3bc2e67de43553fd0b9555
Anderson Serrado <akserrado@gmail.com>
2025-06-06 22:21:33 -0300

    Actualizar L6Q14.py

--- Lista 6/L6Q13.py
+++ Lista 6/L6Q13.py
@@ -1,3 +1,14 @@
+'''
+14) Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+a. Álcool:
+b. até 20 litros, desconto de 3% por litro
+c. acima de 20 litros, desconto de 5% por litro
+d. Gasolina:
+e. até 20 litros, desconto de 4% por litro
+f. acima de 20 litros, desconto de 6% por litro
+Escreva um programa em python algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pagopelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcoolé R$ 1,90.
+'''
+
 # Preços por litro
 cA = 1.90  # preço do litro do álcool
 cG = 2.50  # preço do litro da gasolina
