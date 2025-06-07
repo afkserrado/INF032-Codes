92a428bfe641611cc8b190b45439f529b264f309
Anderson Serrado <akserrado@gmail.com>
2025-06-06 22:52:49 -0300

    Actualizar L6Q12.py

--- Lista 6/L6Q12.py
+++ Lista 6/L6Q12.py
@@ -14,38 +14,39 @@ import math
 
 area = 1
 while area > 0:
-
-    area = float(input("Informe a area (digite 0 ou menor para parar): "))
+    area = float(input("Informe a área (digite 0 ou menor para parar): "))
 
     if area <= 0:
         break
 
     litros = area / 6
-    
-    # Solução 1
-    qLatas = math.ceil(litros / 18)
-    pLatas = qLatas * 80
-    print(f"Apenas latas -> Quantidade: {qLatas} | Custo: {pLatas: .2f}")
 
-    # Solução 2
+    # Solução 1: apenas latas
+    qLatas = math.ceil(litros / 18)
+    precoLatas = qLatas * 80
+    print(f"Apenas latas -> Quantidade: {qLatas} | Custo: R$ {precoLatas:.2f}")
+
+    # Solução 2: apenas galões
     qGaloes = math.ceil(litros / 3.6)
-    pGaloes = qGaloes * 25
-    print(f"Apenas galoes -> Quantidade: {qGaloes} | Custo: {pGaloes: .2f}")
+    precoGaloes = qGaloes * 25
+    print(f"Apenas galões -> Quantidade: {qGaloes} | Custo: R$ {precoGaloes:.2f}")
 
-    # Galões só é vantagem se for até 10,8 litros, que dá 3 galões, custando 75 reais. Acima disso, é mais vantajoso comprar uma lata.
+    # Solução 3: mistura
+    if litros <= 10.8:
+        qLatasMix = 0
+        qGaloesMix = math.ceil(litros / 3.6)
+    else:
+        qLatasMix = math.floor(litros / 18)
+        if qLatasMix == 0:
+            qLatasMix = 1
+        sobra = litros - (qLatasMix * 18)
 
-    # Solução 3
-    qLatas = math.ceil(litros / 18)
-    resto = litros - (qLatas * 18)
-    qGaloes = 0
-    pGaloes = 0
+        if sobra > 0:
+            qGaloesMix = math.ceil(sobra / 3.6)
+        else:
+            qGaloesMix = 0
 
-    if resto > 0:
-        qGaloes = math.ceil(resto / 3.6)
-
-    pLatas = qLatas * 80
-    pGaloes = qGaloes * 25
-
-    print(f"Latas e galoes -> Quantidade latas: {qLatas} | Custo latas: {pLatas: .2f} | Quantidade galoes: {qGaloes} | Preco galoes: {pGaloes: .2f}\n")
+    precoMix = (qLatasMix * 80) + (qGaloesMix * 25)
+    print(f"Combinação -> Latas: {qLatasMix} | Galões: {qGaloesMix} | Custo Total: R$ {precoMix:.2f}\n")
 
 print("Programa encerrado.")
